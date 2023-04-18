b458411f4188216b12a3eb37a1c0cf5d08144801
Dyam Marcano <dyam.marcano@gmail.com>
2023-04-18 18:44:33 -0300

    changes

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	"go.opentelemetry.io/otel/sdk/trace"
 	"log"
 	"net/http"
@@ -10,6 +9,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 	"go.opentelemetry.io/otel"
+	exporter "go.opentelemetry.io/otel/exporters/otlp"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
 type User struct {
@@ -20,85 +21,122 @@ type User struct {
 
 var users = make(map[string]User) // mock database
 
-func createUser(c *gin.Context) {
+func createUserHandler(c *gin.Context) {
+	ctx := c.Request.Context()
+	tracer := otel.Tracer("createUserHandler")
+
 	var user User
 	if err := c.BindJSON(&user); err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	users[user.ID] = user
-	c.JSON(http.StatusOK, gin.H{"status": "created"})
-}
 
-func getUser(c *gin.Context) {
-	userID := c.Param("id")
-	user, ok := users[userID]
-	if !ok {
-		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "user not found"})
-		return
-	}
+	ctx, span := tracer.Start(ctx, "createUser")
+	defer span.End()
+
+	users[user.ID] = user
 	c.JSON(http.StatusOK, user)
 }
 
-func updateUser(c *gin.Context) {
+func getUserHandler(c *gin.Context) {
+	ctx := c.Request.Context()
+	tracer := otel.Tracer("getUserHandler")
+
 	userID := c.Param("id")
+	ctx, span := tracer.Start(ctx, "getUser")
+	defer span.End()
+
 	user, ok := users[userID]
 	if !ok {
-		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "user not found"})
+		c.JSON(http.StatusNotFound, gin.H{"error": "user not found"})
 		return
 	}
-	var updateUser User
-	if err := c.BindJSON(&updateUser); err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	user.Name = updateUser.Name
-	user.Email = updateUser.Email
-	users[userID] = user
-	c.JSON(http.StatusOK, gin.H{"status": "updated"})
+
+	c.JSON(http.StatusOK, user)
 }
 
-func deleteUser(c *gin.Context) {
+func updateUserHandler(c *gin.Context) {
+	ctx := c.Request.Context()
+	tracer := otel.Tracer("updateUserHandler")
+
 	userID := c.Param("id")
+	ctx, span := tracer.Start(ctx, "updateUser")
+	defer span.End()
+
+	var userUpdates User
+	if err := c.BindJSON(&userUpdates); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
 	if _, ok := users[userID]; !ok {
-		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "user not found"})
+		c.JSON(http.StatusNotFound, gin.H{"error": "user not found"})
+		return
+	}
+
+	users[userID] = User{
+		ID:    userID,
+		Name:  userUpdates.Name,
+		Email: userUpdates.Email,
+	}
+
+	c.JSON(http.StatusOK, users[userID])
+}
+
+func deleteUserHandler(c *gin.Context) {
+	ctx := c.Request.Context()
+	tracer := otel.Tracer("deleteUserHandler")
+
+	userID := c.Param("id")
+	ctx, span := tracer.Start(ctx, "deleteUser")
+	defer span.End()
+
+	if _, ok := users[userID]; !ok {
+		c.JSON(http.StatusNotFound, gin.H{"error": "user not found"})
 		return
 	}
 
 	delete(users, userID)
-
 	c.JSON(http.StatusOK, gin.H{"status": "deleted"})
 }
 
-func tracer() *trace.TracerProvider {
+func listUseresHandler(c *gin.Context) {
+	ctx := c.Request.Context()
+	tracer := otel.Tracer("listUseresHandler")
+
+	ctx, span := tracer.Start(ctx, "listUseres")
+	defer span.End()
+
+	c.JSON(http.StatusOK, users)
+}
+
+func tracer() (tp *trace.TracerProvider) {
 	// Initialize the tracer
 	exporter, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	tp := trace.NewTracerProvider(
+	tp = trace.NewTracerProvider(
 		trace.WithBatcher(exporter),
 		trace.WithSampler(trace.AlwaysSample()),
 	)
-
-	return tp
+	return
 }
 
 func main() {
 	r := gin.Default()
-
-	tp := tracer()
-	otel.SetTracerProvider(tp)
+	otel.SetTracerProvider(tracer())
 
 	// Use the OpenTelemetry middleware for Gin
 	r.Use(otelgin.Middleware("my-api"))
 
 	// Define CRUD routes
-	r.POST("/users", createUser)
-	r.GET("/users/:id", getUser)
-	r.PUT("/users/:id", updateUser)
-	r.DELETE("/users/:id", deleteUser)
+	r.Handle(http.MethodPost, "/users", createUserHandler)
+	r.Handle(http.MethodGet, "/users/:id", getUserHandler)
+	r.Handle(http.MethodPut, "/users/:id", updateUserHandler)
+	r.Handle(http.MethodGet, "/users", listUseresHandler)
+	r.Handle(http.MethodDelete, "/users/:id", deleteUserHandler)
 
 	fmt.Println("Server is running on port 8080")
 
